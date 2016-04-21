f4c612950cd8fd31acb0e24135ddb01f5dbb37f6
yanguoqi <yanzi@yanzideMac-mini.local>
2016-04-21 15:29:19 +0800

    完善，支持多url输入

--- spider.py
+++ spider.py
@@ -1,9 +1,8 @@
 import sys,urllib.request, os
-from selenium import webdriver
 from bs4 import BeautifulSoup
 # python /Users/yanzi/work/workspaces/python/spider.py http://www.baidu.com
 # print(sys.argv[0])
-# print(urllib.__file__)
+print(urllib.__file__)
 
 # 获取脚本文件的当前路径
 def cur_file_dir():
@@ -20,6 +19,28 @@ def get_source_url():
     else:
         return url
 
+def get_template_body():
+    # 加载模版
+    path = cur_file_dir() + "/template.html"
+    soup2 = BeautifulSoup(open(path), "html.parser")
+    body = soup2.body
+    return (soup2, body)
+
+def get_answer(url):
+    content = urllib.request.urlopen(url).read()
+# fp = open("/Users/yanzi/Desktop/aaa.html", "wb")
+# fp.write(content)
+# fp.close()
+    soup = BeautifulSoup(content, "html.parser")
+# print(soup.title)
+    answer1 = soup.find('div', class_='single-siti clearfix')
+    answer1.i.extract()
+    answer2 = soup.find('div', class_='tm-bottom')
+    answer2.a.extract()
+    answer3 = soup.find('div', class_='analysis clearfix')
+    return (answer1, answer2, answer3)
+
+"""
 def get_url_from_browser():
  path = cur_file_dir()
  # dr = webdriver.Chrome(executable_path=path + '/chromedriver')
@@ -37,31 +58,26 @@ def get_url_from_browser2():
  url = dr.current_url
  print(url)
  # url = dr.open_new_tab("http:\\www.163.com")
+ """
 
-url=get_source_url()
-print(url)
-wp = urllib.request.urlopen(url)
-content = wp.read()
-# fp = open("/Users/yanzi/Desktop/aaa.html", "wb")
-# fp.write(content)
-# fp.close()
-soup = BeautifulSoup(content, "html.parser")
-# print(soup.title)
-answer1 = soup.find('div', class_='single-siti clearfix')
-answer2 = soup.find('div', class_='tm-bottom')
-answer3 = soup.find('div', class_='analysis clearfix')
+# url=get_source_url()
+(soup2, body) = get_template_body();
+url = input(".....请输入url地址,多个url以空格分开....\n")
+# print(url)
+urls = url.split(" ")
+for temp_url in urls:
+    (a1, a2, a3) = get_answer(temp_url)
+    body.append(a1)
+    body.append(a2)
+    body.append(a3)
+    tt = BeautifulSoup("<br/>", "html.parser")
+    body.append(tt)
 
-# 加载模版
-path = cur_file_dir() + "/template.html"
-soup2 = BeautifulSoup(open(path), "html.parser")
-body = soup2.body
-body.append(answer1)
-body.append(answer2)
-body.append(answer3)
+#save to answer.html
 fp = open("/Users/yanzi/Desktop/answer.html", "w")
 fp.write(soup2.prettify())
 fp.close()
-print('That is ok...')
+print('-----------------That is ok---------------')
 
 
 
